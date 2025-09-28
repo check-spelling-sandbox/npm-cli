388723e11daecc892c4eb2508c7416ec9373a7ab
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-28 19:04:03 -0400

    spelling: don't
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- workspaces/arborist/test/arborist/build-ideal-tree.js
+++ workspaces/arborist/test/arborist/build-ideal-tree.js
@@ -1129,7 +1129,7 @@ t.test('resolve links in global mode', async t => {
   t.equal(tree.children.get('linked-dep').resolved, resolved)
 })
 
-t.test('dont get confused if root matches duped metadep', async t => {
+t.test('don't get confused if root matches duped metadep', async t => {
   createRegistry(t, true)
   const path = resolve(fixtures, 'test-root-matches-metadep')
   const arb = newArb(path, { installStrategy: 'hoisted' })
